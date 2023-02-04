b4724ca3e6d5d2f14fb2d0d0cde82617911d23df
chemezoida <evchemez@mail.ru>
2023-02-05 01:06:28 +0300

    count product in cart

--- js/main.js
+++ js/main.js
@@ -1,32 +1,60 @@
+//создаём переменные  для записи товаров в корзину И избранное, переменную для подсчёта товаров в корзине. Точнее id товаров
+//и записываем в них значения localStorage
+let arrayCart = window.localStorage.getItem('cart');
+let arrayHeart = window.localStorage.getItem('heart');
+let countProduct = window.localStorage.getItem('countCart');
 
 
+//если нет сохранённого, то создаём новые переменные. 
+if (arrayCart == null){
+    arrayCart = new Array();
+}else {arrayCart = JSON.parse(arrayCart);}
+
+if (arrayHeart == null){
+    arrayHeart = new Array();
+}else {arrayHeart = JSON.parse(arrayHeart);}
+
+if (countProduct == null){
+    countProduct = 0;
+}
+
+
+//впишем в красный круг корзины количество товаров из переменной countCart
+document.getElementById('countProduct').innerHTML = countProduct;
 
 
 //переменные для записи элементов HTML
+//основной контейнер, куда будут отрисовываться все страницы.
 let containerPage = document.getElementById('containerPage');
 
+let containerCountProduct = document.getElementById('countProduct');
+
+//шаблоны для отрисовки
 let templateCatalog = document.getElementById('tmpl-catalog').innerHTML;
 let templateCard = document.getElementById('tmpl-card').innerHTML;
 let templateMainContent = document.getElementById('mainContent').innerHTML;
+let templateCart = document.getElementById('tmpl-cart').innerHTML;
 
 //let templateDeliveryPay = document.getElementById('tmpl-deliveryPay').innerHTML;
 //let templateContacts = document.getElementById('tmpl-contacts').innerHTML;
-let templateCart = document.getElementById('tmpl-cart').innerHTML;
 
-//отрисуем при загрузке страницу путём вызова функции
+
+//отрисуем при загрузке Главную страницу путём вызова функции
 renderMainPage();
+
 
 //функция очистки страницы
 function clearPage(){
-
     containerPage.innerHTML="";
 }
+
 
 //функция вывода главной страницы
 function renderMainPage(){
     clearPage();
     containerPage.innerHTML = templateMainContent;
 }
+
 
 //функция для отправки запросов
 function sendRequestGET(url){
@@ -36,9 +64,11 @@ function sendRequestGET(url){
     return requestObj.responseText;
 }
 
-  //функция отрисовки каталога
-  function renderCatalog(){
+
+//функция отрисовки каталога
+function renderCatalog(){
     clearPage();
+
     //получаем данные каталога
     let json = sendRequestGET('http://localhost:8090/api/get/?table=Goods');
     //раскодируем данные
@@ -47,19 +77,19 @@ function sendRequestGET(url){
     console.log(data);
 
     //используем шаблон html - tmpl-catalog для вывода на страницу всех товаров из API
-    containerPage.style.backgroundColor="";
+ //   containerPage.style.backgroundColor="";
 
-     for (let i = 0; i < data.length; i++){   
+        for (let i = 0; i < data.length; i++){   
         containerPage.innerHTML += templateCatalog.replace('${id}', data[i]['id'])
-                                                  .replace('${title}', data[i]['product_name'])
-                                                  .replace('${photo}', data[i]['image'])
-                                                  .replace('${price}', data[i]['price']);
-     }
-   
- }   
+                                                    .replace('${title}', data[i]['product_name'])
+                                                    .replace('${photo}', data[i]['image'])
+                                                    .replace('${price}', data[i]['price']);
+        }
+}   
 
- //функция отрисовки карточки
- function renderCard(id){
+
+//функция отрисовки карточки
+function renderCard(id){
 
     clearPage();
 
@@ -68,10 +98,10 @@ function sendRequestGET(url){
     //раскодируем данные
     let data = JSON.parse(json);
 
-        console.log(data);
-        console.log(data[0]['id']);
+    console.log(data);
+    console.log(data[0]['id']);
 
-    /*вот это /{id}/g нужно для того, чтобы заменить ВСЕ найденные значения {id}, а не первое */
+    //меняем данные в шаблоне данными из апишки
     containerPage.innerHTML += templateCatalog.replace('${id}', data[0]['id'])
                                             .replace('${title}', data[0]['product_name'])
                                             .replace('${photo}', data[0]['image'])
@@ -80,4 +110,15 @@ function sendRequestGET(url){
                                             .replace('{count}', 'число')
                                             .replace('{description}', data[0]['product_description']);
                                    
-  }
+}
+
+  
+
+//добавление товара в корзину (записываем в ls только id)
+//ПОКА ДОБАВЛЕНИЯ НЕТ, ТОЛЬКО ПОДСЧЁТ В КРУЖОЧКЕ, СКОЛЬКО РАЗ НАЖАЛИ НА ADD
+function addProductInCart(){
+
+    countProduct++;
+    containerCountProduct.innerHTML = countProduct;
+
+}
